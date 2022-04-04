8b9bb2340328aa833199c10d4e805f6c1559c528
Shreshsuman <76208040+Shreshsuman@users.noreply.github.com>
2022-04-04 15:53:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    background("white");
+}
+
+function draw(){
+}
+
+function clear_canvas(){
+    background("white");
+}
